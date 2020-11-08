6bcf94d2f318493c5b3f752b2020d2699c7308fc
Ravi <rvnarwal13@gmail.com>
2020-11-08 17:13:16 +0530

    first commit

---
+++ js/settings.js
@@ -0,0 +1,15 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      document.getElementsByClassName("para").style.color="hsl(240, 5%, 91%)";
+    } 
+  });
+}
